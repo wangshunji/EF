f35e96f693b055811dc10b1828230a8b269be6f0
wangshunji <wangshunji@126.com>
2018-07-14 12:18:19 +0800

    增加部分映射属性

---
+++ Discriminator/Discriminator/Model/Test_PriKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discriminator.Model{
+
+    /// <summary>
+    /// 手动配置主键，测试实体
+    /// </summary>
+    public class Test_PriKey{
+        public int DD { get; set; }
+        public string SS { get; set; }
+
+    }
+
+}
---
+++ Discriminator/Discriminator/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discriminator.Model{
+
+    public class User{
+        /// <summary>
+        /// 主键，用户ID
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 用户的真实姓名
+        /// </summary>
+        public string RealName { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public int Phone { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+
+}
